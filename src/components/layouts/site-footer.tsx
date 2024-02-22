28ba883a8e081b341b2f5d381c67f5706bb09bc8
Hamza <hamza22zirar@gmail.com>
2024-02-22 17:45:37 +0100

    added the navbar and the footer layouts

--- src/components/layouts/site-footer.tsx
+++ src/components/layouts/site-footer.tsx
@@ -1,20 +1,17 @@
 export default function SiteFooter() {
   return (
-    <div className="md:container md:max-w-6xl px-4 bg-slate-500">
-      <div className="fixed bottom-8 select-none flex justify-between md:max-w-6xl w-full items-center  0 pr-8">
-        <div className="flex-1 text-left text-sm leading-loose text-muted-foreground">
-          Built by{" "}
-          <a
-            href="https://ousama.vercel.app"
-            target="_blank"
-            rel="noreferrer"
-            className="font-semibold transition-colors hover:text-foreground"
-          >
-            Maxima Communication
-          </a>
-          .
+    <footer className="flex flex-col items-center gap-8 bg-slate-400 mt-4 pt-7">
+      <div className="flex  justify-around w-full">
+        <div className="">
+          <p>newsletter</p>
+        </div>
+        <div className="">
+          <p>follow us</p>
         </div>
       </div>
-    </div>
+      <div className="h-12">
+        <p>made by</p>
+      </div>
+    </footer>
   );
 }
--- src/components/layouts/site-header.tsx
+++ src/components/layouts/site-header.tsx
@@ -1,20 +1,20 @@
 "use client";
 
-import { ThemeToggle } from "@/components/theme-toggle";
-import { Button, buttonVariants } from "@/components/ui/button";
-import useWindow from "@/hooks/use-window";
-import { Github } from "lucide-react";
+import { ArrowRight } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import maxima from "../../../public/maxima-logo.svg";
+import localFont from "next/font/local";
+
+const Bogart = localFont({
+  src: "../../../public/assets/fonts/bogart/Bogart-SemiBold-trial.ttf",
+});
 
 export default function SiteHeader() {
-  const { isDesktop } = useWindow();
-  const path = usePathname();
   return (
     <header className="md:container md:max-w-6xl px-4">
       <nav className="md:py-8 py-4 flex w-full justify-between items-center z-50">
+        {/* --------------------------------- LOGO ------------------------------------------ */}
         <h2 className="text-2xl font-semibold tracking-wide flex gap-2.5 items-center">
           <>
             <Link href={"/"}>
@@ -26,27 +26,43 @@ export default function SiteHeader() {
             </Link>
           </>
         </h2>
-        <div className="flex content-center justify-between ">
-          <div className="border border-white ">
-            <div className="flex justify-center items-center gap-2">
-              <Link
-                target="_blank"
-                href="http://github.com/sujjeee/codox"
-                className={buttonVariants({
-                  size: "icon",
-                  variant: "outline",
-                })}
+        {/* ------------------------------------------------------------------------------------- */}
+
+        {/* ---------------------------------------------NavBar---------------------------------------------------- */}
+        <div className="border  rounded-full px-6 py-3 bg-gray-800">
+          <div className="flex justify-center items-center gap-12  ">
+            <Link href="#">
+              <p
+                className={` border rounded-full bg-slate-500 -mx-3 -my-1 px-3 py-2 w-[130px] text-center ${Bogart.className}`}
               >
-                <Github className="h-5 w-5" />
-                <span className="sr-only">github profile</span>
-              </Link>
-              <ThemeToggle />
-              <Button className="flex" asChild>
-                <Link href="/signin">Sign in</Link>
-              </Button>
-            </div>
+                Our Services
+              </p>
+            </Link>
+            <Link href="#">
+              <p
+                className={` border rounded-full bg-slate-500 -mx-3 -my-1 px-3 py-2 w-[130px] text-center ${Bogart.className}`}
+              >
+                Our Process
+              </p>
+            </Link>
+            <Link href="#">
+              <p
+                className={` border rounded-full bg-slate-500 -mx-3 -my-1 px-3 py-2 w-[130px] text-center ${Bogart.className}`}
+              >
+                About us
+              </p>
+            </Link>
           </div>
         </div>
+        {/*-------------------------------------------------------------------------------------------------- */}
+        {/* -------------------------------------------COntact Us ----------------------*/}
+        <div className="w-44 ">
+          <div className="flex justify-around border rounded-full bg-white text-black  px-5 py-3 text-center font-semibold ">
+            <p className="mr-3">Contact Us</p>
+            <ArrowRight className="w-4 h-6" />
+          </div>
+        </div>
+        {/*------------------------------------------------------------------------------------- */}
       </nav>
     </header>
   );
