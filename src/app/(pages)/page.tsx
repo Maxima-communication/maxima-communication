f8886fcd67701cd163a32143aac22f1c20a28c93
ousama <ousawack@gmail.com>
2024-03-08 17:45:54 +0100

    added fonts, styled up the hero section & integreted the globe component

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,23 +1,42 @@
 import localFont from "next/font/local";
+import Globe from "@/components/globe";
 
 const Bogart = localFont({
   src: "../../../public/assets/fonts/bogart/Bogart-SemiBold-trial.ttf",
 });
 
+const Glockenspiel = localFont({
+  src: "../../../public/assets/fonts/LT Glockenspiel Black.ttf",
+});
+
+const Relevance = localFont({
+  src: "../../../public/assets/fonts/Relevance-trial-Regular.ttf",
+});
+
 export default function Home() {
   return (
     <main>
-      <div className="h-[70vh] w-full justify-center items-center flex px-4  container max-w-6xl">
+      <div className="h-[70vh] w-full justify-center items-center flex px-4 container max-w-6xl relative">
         {/*  <div className={Bogart.className}>
         <div className="scroll-m-20 text-2xl sm:text-3xl md:text-4xl font-extrabold tracking-normal lg:text-5xl text-center space-y-3 justify-center w-2/3">
           Experience Our Modern web development {<br />}
         </div>
       </div> */}
-        <div className="w-3/5 text-center text-2xl font-extrabold ">
-          Experience Our Modern web development
+        <div className="w-3/5 -mt-48 space-y-5">
+          <div
+            className={`leading-tight tracking-wide text-5xl text-gray-50 ${Glockenspiel.className}`}
+          >
+            EXPERIENCE OUR EXPERT AGENCY SERVICES
+          </div>
+          <div
+            className={`text-xl text-gray-300 leading-relaxed ${Relevance.className}`}
+          >
+            We are a digital agency that specializes in building and scaling
+            cutting-edge digital products for startups and industrial giants
+          </div>
         </div>
-        <div className="h-full w-2/5 flex items-center justify-center bg-slate-700 ">
-          The GLOBE
+        <div className="h-full w-2/5 flex items-center justify-center ">
+          <Globe />
         </div>
       </div>
       <div className="w-full bg-slate-400 text-center h-16">wow</div>
