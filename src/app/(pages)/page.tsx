f4cda1cb126c03bd11a24eb93b5fe676d2484504
Hamza <hamza22zirar@gmail.com>
2024-03-15 22:21:32 +0000

    fixed the globe ratios and position

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -16,27 +16,29 @@ const Relevance = localFont({
 export default function Home() {
   return (
     <main>
-      <div className="h-[70vh] w-full justify-center items-center flex px-4 container max-w-6xl relative">
+      <div className="h-[70vh] w-full justify-center items-center flex px-4 container max-w-6xl relative  ">
         {/*  <div className={Bogart.className}>
         <div className="scroll-m-20 text-2xl sm:text-3xl md:text-4xl font-extrabold tracking-normal lg:text-5xl text-center space-y-3 justify-center w-2/3">
           Experience Our Modern web development {<br />}
         </div>
       </div> */}
-        <div className="w-3/5 -mt-48 space-y-5">
-          <div
-            className={`leading-tight tracking-wide text-5xl text-gray-50 ${Glockenspiel.className}`}
-          >
-            EXPERIENCE OUR EXPERT AGENCY SERVICES
+        <div className="flex items-center  ">
+          <div className="w-3/5 -mt-28 space-y-5">
+            <div
+              className={`leading-tight tracking-wide text-5xl text-gray-50 ${Glockenspiel.className}`}
+            >
+              EXPERIENCE OUR EXPERT AGENCY SERVICES
+            </div>
+            <div
+              className={`text-xl text-gray-300 leading-relaxed ${Relevance.className}`}
+            >
+              We are a digital agency that specializes in building and scaling
+              cutting-edge digital products for startups and industrial giants
+            </div>
           </div>
-          <div
-            className={`text-xl text-gray-300 leading-relaxed ${Relevance.className}`}
-          >
-            We are a digital agency that specializes in building and scaling
-            cutting-edge digital products for startups and industrial giants
+          <div className="absolute top-0 right-0 h-full w-[800px] overflow-visible -mr-44">
+            <Globe />
           </div>
-        </div>
-        <div className="h-full w-2/5 flex items-center justify-center ">
-          <Globe />
         </div>
       </div>
       <div className="w-full bg-slate-400 text-center h-16">test test</div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -80,7 +80,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
