e185a4572aed040c8346b620c9c5e93f0915594a
Hamza <hamza22zirar@gmail.com>
2024-03-19 21:57:55 +0000

    integrated the Process component into the main page

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -2,6 +2,7 @@ import localFont from "next/font/local";
 import Globe from "@/components/globe";
 import Ticker from "@/components/ticker";
 import Services from "@/components/Services";
+import Process from "@/components/Process";
 
 const Bogart = localFont({
   src: "../../../public/assets/fonts/bogart/Bogart-SemiBold-trial.ttf",
@@ -26,6 +27,8 @@ export default function Home() {
         </div>
       </div> */}
           <div className="flex items-center  ">
+            {/* ----------------------------------- HERO SECTION ---------------------------------------------------- */}
+            
             <div className="w-3/5 -mt-28 space-y-5">
               <div
                 className={`leading-tight tracking-wide text-5xl text-gray-50 ${Glockenspiel.className}`}
@@ -39,17 +42,25 @@ export default function Home() {
                 cutting-edge digital products for startups and industrial giants
               </div>
             </div>
+            {/* ----------------------------------- THE GLOBE ---------------------------------------------------- */}
+
             <div className="absolute top-0 right-0 h-full w-[800px] overflow-visible -mr-44">
               <Globe />
             </div>
           </div>
         </div>
       </div>
+      {/* ----------------------------------- Ticker ---------------------------------------------------- */}
+
       <div className="w-full text-gray-50 text-4xl font-semibold h-16 -mt-10  flex items-center">
         <Ticker />
       </div>
+
       {/* ------------------------------------ Services ------------------------------------------------- */}
       <Services />
+
+      {/* ------------------------------------ OUR PROCESS ---------------------------------------------- */}
+      <Process />
     </main>
   );
 }
