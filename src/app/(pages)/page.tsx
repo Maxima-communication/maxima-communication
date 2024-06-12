345c6bb1472e211968c2d9ab29f38a0e1b567a5b
ousama <ousawack@gmail.com>
2024-06-12 11:55:47 +0100

    styling & font

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -13,30 +13,43 @@ const Hagrid = localFont({
   src: "../../../public/assets/fonts/hagrid/Hagrid-Text-Extrabold-trial.ttf",
 });
 
+// Testing font
+const TestFont = localFont({
+  src: "../../../public/assets/fonts/area/AreaInktrapTrial-Extrabold-BF65ea75c482e6b.otf",
+});
+
+// Flipped font
+const FlippedFont = localFont({
+  src: "../../../public/assets/fonts/distant_feeling/Distant Feeling-Italic.ttf",
+});
+
 export default function Home() {
   return (
     <main>
-        <div className="h-[70vh] w-full justify-center items-center flex px-4 container max-w-6xl relative">
-            <div className="w-4/5 -mt-28 space-y-5 text-center">
-              <h1
-                className={`leading-tight tracking-wider text-7xl text-gray-100 ${Hagrid.className}`}
-              >
-                EXPERIENCE OUR EXPERT AGENCY SERVICES
-              </h1>
-              <h6
-                className={`text-xl text-gray-300 leading-relaxed ${Relevance.className}`}
-              >
-                We are a digital agency that specializes in building and scaling
-                cutting-edge digital products for startups and industrial giants
-              </h6>
-          </div>
+      <div className="h-[70vh] w-full my-14 justify-center items-center flex px-4 container max-w-6xl relative">
+        <div className="w-4/5 -mt-28 space-y-5 text-center">
+          <h1
+            className={`leading-tighter text-9xl text-white opacity-95 ${TestFont.className}`}
+          >
+            Voice your{` `}
+            <span className={`${FlippedFont.className}`}>
+            vision
+            </span>
+          </h1>
+          <h6
+            className={`text-xl text-gray-200 leading-relaxed ${Relevance.className}`}
+          >
+            We are a digital agency that specializes in building and scaling
+            cutting-edge digital products for startups and industrial giants
+          </h6>
         </div>
+      </div>
       <div className="w-full text-gray-50 text-4xl font-semibold h-16 flex items-center">
         <Ticker />
       </div>
       <div className="h-full relative w-full flex flex-col items-center justify-center">
         <div className="relative z-20">
-        <Services />
+          <Services />
           <Aboutus />
           <Process />
 
