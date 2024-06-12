d565cb137ecb134108da414bb14a32ddb2614109
ousama <ousawack@gmail.com>
2024-06-12 11:56:57 +0100

    styling & font

--- src/components/ticker.tsx
+++ src/components/ticker.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+import localFont from "next/font/local";
 import { Icons } from "./icons";
+
+// Testing font
+const TestFont = localFont({
+  src: "../../public/assets/fonts/tabarra/TabarraPro-Black-FFP.ttf",
+});
 
 const Ticker = () => {
   return (
-    <div className="marquee">
-      <ul className="marquee__content">
+    <div className={`marquee ${TestFont.className}`}>
+      <ul className="marquee__content text-5xl">
+        <li>DISCUSS YOUR IDEAS</li>
+        <li>
+          <Icons.star />
+        </li>
         <li>DISCUSS YOUR IDEAS</li>
         <li>
           <Icons.star />
@@ -19,7 +29,11 @@ const Ticker = () => {
         </li>
       </ul>
       {/* Mirrors the content above */}
-      <ul className="marquee__content" aria-hidden="true">
+      <ul className="marquee__content text-5xl" aria-hidden="true">
+        <li>DISCUSS YOUR IDEAS</li>
+        <li>
+          <Icons.star />
+        </li>
         <li>DISCUSS YOUR IDEAS</li>
         <li>
           <Icons.star />
