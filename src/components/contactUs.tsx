4c4998d00f12e45badb76cba47c4cde13632ac0b
Hamza <hamza22zirar@gmail.com>
2024-06-22 17:06:13 +0100

    added the image background and fixed the ratios of the texts in the contact form (almost finished)

--- src/components/contactUs.tsx
+++ src/components/contactUs.tsx
@@ -1,55 +1,89 @@
 import React from "react";
 import contactUs from "@/images/contactUs.svg";
 import Image from "next/image";
+import first from "../images/markus.jpg";
 const ContactUs = () => {
   return (
-    <div className="container max-w-6xl flex items-center my-16 justify-center h- bg--800 text-white relative border border-red-300 -z-20">
-        <div className="absolute inset-0 left-0  h-full ">
-            <Image
-              src={contactUs}
-              alt="wpw"
-              layout="fill"
-
-              className="-z-10 bg--300 object-cover"
-            />
+    <div
+      className="container relative flex flex-col bg-cover bg-center mb-28"
+      /* style={{ backgroundImage: `url(${})` }} */
+    >
+      <div className="absolute top-0 right-0  w-full h-full">
+        <Image
+          src={first}
+          alt="Mesh Background"
+          layout="fill"
+          objectFit="cover"
+          className="blur-[8.6px] z-[-1] container overflow-hidden"
+        />
+      </div>
+      <div className="flex">
+        {/* ------------------------------------ Part 1 --------------------------------------------------- */}
+        <div className="w-1/2 relative  p-10">
+          <div className="text-white flex flex-col space-y-20 my-10 mx-20 p-10">
+            <div className="flex flex-col space-y-6">
+              <p className="font-bold text-5xl leading-tight">
+                Have a project in mind?
+              </p>
+              <p className="font-semibold text-lg leading-relaxed">
+                Reach out to us. We can make something awesome together.
+              </p>
+            </div>
+            <div className="flex flex-col space-y-6">
+              <div>
+                <p className="font-bold text-xl mb-1">ADDRESS</p>
+                <p className="text-lg">Agadir</p>
+              </div>
+              <div>
+                <p className="font-bold text-xl mb-1">EMAIL</p>
+                <p className="text-lg">maximaCom@gmail.com</p>
+              </div>
+              <div>
+                <p className="font-bold text-xl mb-1">PHONE</p>
+                <p className="text-lg">+212 661 683 415</p>
+              </div>
+            </div>
           </div>
-      <div className="flex justify-between gap-10 border px-24 py-28 border-gray-700 relative z-10">
-        {/* ------------------------------------------------ Part 1 ---------------------------------------------------------------- */}
-        <form className="flex flex-col  w-1/2 mr-16 relative z-10 mb-28 max-h-screen ">
-          <input
-            type="text"
-            placeholder="Your Name"
-            className="bg-transparent border-b border-gray-500 p-2  placeholder-gray-500 focus:border-gray-300 focus:outline-none"
-          />
-          <input
-            type="email"
-            placeholder="Your Email"
-            className="bg-transparent border-b border-gray-500 p-2 mt-16  placeholder-gray-500 focus:border-gray-300 focus:outline-none"
-          />
-          <textarea
-            id="thoughts"
-            placeholder="Share your thoughts"
-            className="bg-transparent border-b border-l rounded-bl-md border-gray-500 p-2 mt-16 placeholder-gray-500 focus:outline-none focus:border-gray-300 transition duration-300 h-20"
-          />
-          <button className="bg-blue-500 text-white py-2 px-4 mt-20  rounded hover:bg-blue-700 transition-all duration-2000">
-            SHARE YOUR FEEDBACK
-          </button>
-        </form>
-        
-        {/* ------------------------------------------------ Part 2 ---------------------------------------------------------------- */}
-        <div className="flex flex-col  w-1/2 text-center  ">
-          <h1 className="text-6xl font-bold text-center ">
-            Contact
-          </h1>
-          <div className="flex items-center justify-center space-x-4 my-10 ">
-            <p className="border-t-4 h-2 border-green-400 w-28 "></p>
-            <h2 className="text-5xl font-bold text-center">US</h2>
-          </div>
-          <p className="text-lg text-center max-w-md  ">
-            It is very important for us to keep in touch with you, so we are
-            always ready to answer any question that interests you. Shoot!
-          </p>
         </div>
+        {/* ------------------------------------ Part 2 --------------------------------------------------- */}
+        <div className="w-1/2  border- px-6 py-16 z-50 bg--950 text-white">
+          <div className="mt-8 ml-8">
+          <p className="text-3xl font-bold mb-14 pl-6">Contact Us</p>
+          <form className="flex flex-col w-3/4 pl-6">
+            <input
+              type="text"
+              placeholder="Your Name"
+              className="bg-transparent mb-16 border-b border-gray-500 p-2 placeholder-gray-500 focus:border-gray-300 focus:outline-none"
+            />
+            <input
+              type="text"
+              placeholder="Your Number"
+              className="bg-transparent mb-16 border-b border-gray-500 p-2 placeholder-gray-500 focus:border-gray-300 focus:outline-none"
+            />
+            <input
+              type="email"
+              placeholder="Your Email"
+              className="bg-transparent mb-16 border-b border-gray-500 p-2 placeholder-gray-500 focus:border-gray-300 focus:outline-none"
+            />
+            <textarea
+              id="thoughts"
+              placeholder="Share your thoughts"
+              className="bg-transparent mb-14 border-b border-l rounded-bl-md border-gray-500 p-2 placeholder-gray-500 focus:outline-none focus:border-gray-300 transition duration-300 h-20"
+            />
+          </form>
+          <button className="bg-white text-black ml-6 py-2 px-8 font-semibold hover:bg-gray-400 transition-all duration-300">
+            SEND MESSAGE
+          </button>
+          </div>
+          
+        </div>
+      </div>
+      {/* ------------------------------------ Part 3 --------------------------------------------------- */}
+      <div className="text-white  text-center bg-black bg-opacity-10 p-6">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,
+        totam officiis? Nam omnis id molestias vitae adipisci sed nihil mollitia
+        doloremque atque, optio obcaecati reprehenderit distinctio sint
+        consectetur, corrupti at!
       </div>
     </div>
   );
