ef6eee89df43917bb036f1fc14e810fbb90adea2
Hamza <hamza22zirar@gmail.com>
2024-06-26 22:44:28 +0100

    creating the functions needed to contact us via email / integrating the emailJS

--- src/components/contactUs.tsx
+++ src/components/contactUs.tsx
@@ -1,8 +1,27 @@
-import React from "react";
-import contactUs from "@/images/contactUs.svg";
+"use client";
+import React, { useRef } from "react";
 import Image from "next/image";
 import first from "../images/markus.jpg";
+import emailjs from "@emailjs/browser";
 const ContactUs = () => {
+  const form = useRef<any>(null);
+  const sendEmail = (e: any) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_67y2qdr", "template_3tw03b2", form.current, {
+        publicKey: "INzBYV3etTXU2zl20",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+      form.current.reset();
+  };
   return (
     <div
       className="container relative flex flex-col bg-cover bg-center mb-28"
@@ -48,42 +67,55 @@ const ContactUs = () => {
         {/* ------------------------------------ Part 2 --------------------------------------------------- */}
         <div className="w-1/2  border- px-6 py-16 z-50 bg--950 text-white">
           <div className="mt-8 ml-8">
-          <p className="text-3xl font-bold mb-14 pl-6">Contact Us</p>
-          <form className="flex flex-col w-3/4 pl-6">
-            <input
-              type="text"
-              placeholder="Your Name"
-              className="bg-transparent mb-16 border-b border-gray-500 p-2 placeholder-gray-500 focus:border-gray-300 focus:outline-none"
-            />
-            <input
-              type="text"
-              placeholder="Your Number"
-              className="bg-transparent mb-16 border-b border-gray-500 p-2 placeholder-gray-500 focus:border-gray-300 focus:outline-none"
-            />
-            <input
-              type="email"
-              placeholder="Your Email"
-              className="bg-transparent mb-16 border-b border-gray-500 p-2 placeholder-gray-500 focus:border-gray-300 focus:outline-none"
-            />
-            <textarea
-              id="thoughts"
-              placeholder="Share your thoughts"
-              className="bg-transparent mb-14 border-b border-l rounded-bl-md border-gray-500 p-2 placeholder-gray-500 focus:outline-none focus:border-gray-300 transition duration-300 h-20"
-            />
-          </form>
-          <button className="bg-white text-black ml-6 py-2 px-8 font-semibold hover:bg-gray-400 transition-all duration-300">
-            SEND MESSAGE
-          </button>
+            <p className="text-3xl font-bold mb-14 pl-6">Contact Us</p>
+            <form
+              className="flex flex-col w-3/4 pl-6"
+              ref={form}
+              onSubmit={sendEmail}
+            >
+              <input
+                type="text"
+                required
+                placeholder="Your Name"
+                name="user_name"
+                className="bg-transparent mb-16 border-b border-gray-500 p-2 placeholder-gray-500 focus:border-gray-300 focus:outline-none"
+              />
+              <input
+                type="tel"
+                required
+                placeholder="Your Number"
+                name="user_Number"
+                className="bg-transparent mb-16 border-b border-gray-500 p-2 placeholder-gray-500 focus:border-gray-300 focus:outline-none"
+              />
+              <input
+                type="email"
+                required
+                placeholder="Your Email"
+                name="user_Email"
+                className="bg-transparent mb-16 border-b border-gray-500 p-2 placeholder-gray-500 focus:border-gray-300 focus:outline-none"
+              />
+              <textarea
+                id="thoughts"
+                required
+                placeholder="Share your thoughts"
+                name="message"
+                className="bg-transparent mb-14 border-b border-l rounded-bl-md border-gray-500 p-2 placeholder-gray-500 focus:outline-none focus:border-gray-300 transition duration-300 h-20"
+              />
+              <input
+                type="submit"
+                value="SEND MESSAGE"
+                className="bg-white text-black  py-2 px-8 font-semibold cursor-pointer hover:bg-gray-400 transition-all duration-300"
+              />
+            </form>
           </div>
-          
         </div>
       </div>
       {/* ------------------------------------ Part 3 --------------------------------------------------- */}
       <div className="text-white  text-center bg-black bg-opacity-10 p-6">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,
-        totam officiis? Nam omnis id molestias vitae adipisci sed nihil mollitia
-        doloremque atque, optio obcaecati reprehenderit distinctio sint
-        consectetur, corrupti at!
+        Thank you for considering us for your project. We look forward to
+        collaborating with you to create something extraordinary. Feel free to
+        reach out via the contact form, email, or phone. Our team is here to
+        assist you and answer any questions you may have.
       </div>
     </div>
   );
