85c69eb9f0018d2f978ed9b0d03a00579c390039
ousama <ousamaistaken@gmail.com>
2024-08-25 23:26:38 +0100

    test fix

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,10 +1,15 @@
 "use client";
 
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import dynamic from 'next/dynamic';
 import localFont from "next/font/local";
 import { Element } from "react-scroll";
 import { useLenis } from "lenis/react";
+
+// Disable Lenis for now to see if it's causing issues
+// const Lenis = dynamic(() => import('lenis/react').then(mod => mod.Lenis), {
+//   ssr: false
+// });
 
 const Ticker = dynamic(() => import("@/components/ticker"), { ssr: false });
 const Services = dynamic(() => import("@/components/Services"), { ssr: false });
@@ -14,36 +19,43 @@ const ContactUs = dynamic(() => import("@/components/contactUs"), { ssr: false }
 const AnimatedLogoCloud = dynamic(() => import("@/components/cloud-logo").then(mod => mod.AnimatedLogoCloud), { ssr: false });
 const Hero = dynamic(() => import("@/components/hero"), { ssr: false });
 
-const Relevance = localFont({
-  src: "../../../public/assets/fonts/Relevance-trial-Regular.ttf",
-});
+// Use a different font or adjust the import path
+// const Relevance = localFont({
+//   src: "../../../public/assets/fonts/Relevance-trial-Regular.ttf",
+// });
 
-export default function Home() {
-  const [scrollPosition, setScrollPosition] = useState(0);
+function Page() {
+  useEffect(() => {
+    // Add any necessary effects here
+  }, []);
 
   return (
-    <main className="">
-      <Element name="hero">
-        <Hero />
-      </Element>
-      <AnimatedLogoCloud />
-      <div className="h-full relative w-screen flex md:flex-col md:items-center md:justify-center">
-        <div className="relative z-20">
-          <Element name="services">
-            <Services />
-          </Element>
-          <div className="w-screen text-gray-50 font-semibold h-16 flex items-center pb-36 md:pb-40 lg:pb-24 pt-24 lg:pt-36">
-            <Ticker />
-          </div>
+    <div>
+      {/* <Lenis> */}
+        <Element name="hero">
+          <Hero />
+        </Element>
+        <Element name="services">
+          <Services />
+        </Element>
+        <Element name="process">
+          <Process />
+        </Element>
+        <Element name="aboutus">
           <Aboutus />
-          <Element name="process">
-            <Process />
-          </Element>
-          <Element name="contactus">
+        </Element>
+        <Element name="contactus">
           <ContactUs />
-          </Element>
-        </div>
-      </div>
-    </main>
+        </Element>
+        <Element name="ticker">
+          <Ticker />
+        </Element>
+        <Element name="animated-logo-cloud">
+          <AnimatedLogoCloud />
+        </Element>
+      {/* </Lenis> */}
+    </div>
   );
 }
+
+export default Page;
