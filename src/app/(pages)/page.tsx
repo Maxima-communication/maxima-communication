d33f5f602afec6f37baa6032509b10347e013571
ousama <ousamaistaken@gmail.com>
2024-08-26 03:04:46 +0100

    test fix

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,35 +1,16 @@
 "use client";
 
 import { useEffect } from 'react';
-import dynamic from 'next/dynamic';
-import localFont from "next/font/local";
 import { Element } from "react-scroll";
-import { useLenis } from "lenis/react";
-import Ticker from "@/components/ticker";
-import Services from "@/components/Services";
-import Process from "@/components/Process";
-import Aboutus from "@/components/Aboutus";
-import ContactUs from "@/components/contactUs";
-import {AnimatedLogoCloud} from "@/components/cloud-logo";
-import Hero from "@/components/hero";
+import dynamic from 'next/dynamic';
 
-// Disable Lenis for now to see if it's causing issues
-// const Lenis = dynamic(() => import('lenis/react').then(mod => mod.Lenis), {
-//   ssr: false
-// });
-
-// const Ticker = dynamic(() => import("@/components/ticker"), { ssr: false });
-// const Services = dynamic(() => import("@/components/Services"), { ssr: false });
-// const Process = dynamic(() => import("@/components/Process"), { ssr: false });
-// const Aboutus = dynamic(() => import("@/components/Aboutus"), { ssr: false });
-// const ContactUs = dynamic(() => import("@/components/contactUs"), { ssr: false });
-// const AnimatedLogoCloud = dynamic(() => import("@/components/cloud-logo").then(mod => mod.AnimatedLogoCloud), { ssr: false });
-// const Hero = dynamic(() => import("@/components/hero"), { ssr: false });
-
-// Use a different font or adjust the import path
-// const Relevance = localFont({
-//   src: "../../../public/assets/fonts/Relevance-trial-Regular.ttf",
-// });
+const Ticker = dynamic(() => import("@/components/ticker"), { ssr: false });
+const Services = dynamic(() => import("@/components/Services"), { ssr: false });
+const Process = dynamic(() => import("@/components/Process"), { ssr: false });
+const Aboutus = dynamic(() => import("@/components/Aboutus"), { ssr: false });
+const ContactUs = dynamic(() => import("@/components/contactUs"), { ssr: false });
+const AnimatedLogoCloud = dynamic(() => import("@/components/cloud-logo").then(mod => mod.AnimatedLogoCloud), { ssr: false });
+const Hero = dynamic(() => import("@/components/hero"), { ssr: false });
 
 function Page() {
   useEffect(() => {
@@ -55,7 +36,7 @@ function Page() {
             <Process />
           </Element>
           <Element name="contactus">
-          <ContactUs />
+            <ContactUs />
           </Element>
         </div>
       </div>
